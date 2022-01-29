3a017faec8605cb25429c0e4d2596de08062428c
WelligtomGAlmeida <welligtomgalmeida@gmail.com>
2022-01-29 13:28:16 -0300

    Creating the Product Model and the Customer Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('Name', max_length=100)
+    price = models.DecimalField('Price', decimal_places=2, max_digits=8)
+    stock = models.IntegerField('Units in Stock')
+
+
+class Customer(models.Model):
+    name = models.CharField('Name', max_length=100)
+    lastName = models.CharField('Last Name', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
