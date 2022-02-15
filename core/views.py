89e872f802d3e1cfe3f11c12fb90b11e8943bad3
WelligtomGAlmeida <welligtomgalmeida@gmail.com>
2022-02-14 22:01:26 -0300

    Showing the data using the templates

--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def index(request):
+    products = Product.objects.all()
+
     context = {
-        'course': 'Web Programming with Django Framework'
+        'course': 'Web Programming with Django Framework',
+        'products': products
     }
 
     return render(request, 'index.html', context)
@@ -11,3 +15,11 @@ def index(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def product(request, id):
+    prod = Product.objects.get(id=id)
+    context = {
+        'product': prod
+    }
+    return render(request, 'product.html', context)
